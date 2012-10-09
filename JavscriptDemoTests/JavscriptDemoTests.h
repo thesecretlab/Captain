438b1cda92fe83b67a27ac00d070f1ec13d70e4f
Jon Manning <jon@secretlab.com.au>
2012-10-09 17:37:23 +1100

    Initial Commit

---
+++ JavscriptDemoTests/JavscriptDemoTests.h
@@ -0,0 +1,13 @@
+//
+//  JavscriptDemoTests.h
+//  JavscriptDemoTests
+//
+//  Created by Jon Manning on 9/10/12.
+//  Copyright (c) 2012 Secret Lab. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JavscriptDemoTests : SenTestCase
+
+@end
---
+++ JavscriptDemoTests/JavscriptDemoTests.m
@@ -0,0 +1,32 @@
+//
+//  JavscriptDemoTests.m
+//  JavscriptDemoTests
+//
+//  Created by Jon Manning on 9/10/12.
+//  Copyright (c) 2012 Secret Lab. All rights reserved.
+//
+
+#import "JavscriptDemoTests.h"
+
+@implementation JavscriptDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in JavscriptDemoTests");
+}
+
+@end
